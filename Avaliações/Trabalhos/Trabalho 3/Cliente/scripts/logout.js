0da398ed45fe31793a44e778c13cc8e9b3d8fc05
Jéssica Ferraz <106453893+JessicaFerraz13@users.noreply.github.com>
2025-02-01 04:16:00 -0300

    update trabalho 3

--- Avaliações/Trabalhos/Trabalho 3/Cliente/scripts/logout.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('logoutBtn').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    localStorage.removeItem('access_token');
-
-    window.location.href = '../index.html';
-});
